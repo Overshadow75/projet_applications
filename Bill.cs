5104f650cf10657a51599742a91aa386ab93c5f3
Sylvain M <syl.migeon@gmail.com>
2022-10-05 18:50:57 +0200

    Last Classes Added

---
+++ Bill.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ProjetApplication
+{
+    class Bill
+    {
+        private int orderID;
+        private float price;
+        private int metierID;
+        private int personID;
+        private Boolean cashed = false;
+    }
+}
---
+++ Delivery.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetApplication
+{
+    class Delivery
+    {
+        private int oderID;
+        private Boolean state = false;
+        private float money_received;
+    }
+}
