fdf8e86f6ef3f570520e13e0d3304c3ca804c585
NDU <61319301+Overshadow75@users.noreply.github.com>
2022-10-05 17:09:38 +0200

    classe address

---
+++ address.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ProjetApplication
+{
+    class Address
+    {
+        public int number;
+        public string streetName = "";
+        public string city = "";
+        public string zipCode = "";
+        public string country = "";
+    }
+}
